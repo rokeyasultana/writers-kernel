912cadc675cc0b290589f97b977c7df14db33dea
jakiasultana2022 <rokeyasbd@gmail.com>
2022-07-08 10:44:22 +0600

    initial setup for writers kernel

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1093fd",
+          secondary: "#eb3c62",
+          accent: "#000000",
+          neutral: "#bf00ff",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
