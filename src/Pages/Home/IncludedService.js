26cf32b78e94f8b4c0bd157b27df65f7cccf25e5
jakiasultana2022 <rokeyasbd@gmail.com>
2022-07-13 23:18:37 +0600

    style

--- src/Pages/Home/IncludedService.js
+++ src/Pages/Home/IncludedService.js
@@ -5,14 +5,14 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 const IncludedService = () => {
     return (
-        <div className='mt-10
-        mb-15'>
+        <div className='justify-center mt-10
+        mb-15 ml-7 '>
            <h1 className='text-4xl  text-blue-700'>Our Website Content Writing Services Includes</h1> 
-           <div class="  ">
+           <div>
   <div class="card-body mt-9 ">
       
-  <div className='text-left  mt-2 ml-20  '>
-           <p className='mt-2'> <FontAwesomeIcon icon={faCircleCheck}  color='blue'  /> Writing articles: for your blogs, your showcases, your e-commerce sites, etc…</p>
+  <div className=' text-left mt-2'>
+            <p className='mt-2'> <FontAwesomeIcon icon={faCircleCheck}  color='blue'  /> Writing articles: for your blogs, your showcases, your e-commerce sites, etc…</p>
             <p className='mt-2'> <FontAwesomeIcon icon={faCircleCheck}  color='blue' /> Product review article writing: for amazon affiliate sites.</p>
             <p className='mt-2'> <FontAwesomeIcon icon={faCircleCheck}  color='blue'  /> E-commerce product description writing services.</p>
             <p className='mt-2'> <FontAwesomeIcon icon={faCircleCheck}  color="blue"  /> Professional English proofreading: for affiliate articles, book, journal, articles, blog post, eBooks.</p>
